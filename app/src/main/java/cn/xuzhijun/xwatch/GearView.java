c8e2a1c882f142d160e2aca52b6642c7be810e5e
x1388923662j <xuzhijun012@qq.com>
2018-04-13 11:20:42 +0800

    ii

--- app/src/main/java/cn/xuzhijun/xwatch/GearView.java
+++ app/src/main/java/cn/xuzhijun/xwatch/GearView.java
@@ -25,14 +25,14 @@ public class GearView extends View {
 	
 	private int width, height, r, lon;
 	
-	private Canvas gearCanvas;
+	//	private Canvas gearCanvas;
 	
 	private Xfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_OUT);
 	private Bitmap gear;
 	
 	private int     gearHeight     = DisplayUtil.dp2px(15);
 	private double  transferRadio  = 0;
-	private int     gearToothCount = 112;
+	private int     gearToothCount = 53;
 	private boolean directionRight = true;
 	private double  perDegree      = 30;
 	private int     gearColor      = Color.BLACK;
@@ -40,6 +40,7 @@ public class GearView extends View {
 	private float   strokeWidth    = 1;
 	private int     strokeColor    = Color.WHITE;
 	private boolean showSideHole   = true;
+	private boolean showCenterHole = true;
 	
 	private long startTime;
 	
@@ -73,6 +74,7 @@ public class GearView extends View {
 			strokeWidth = typedArray.getFloat(R.styleable.GearView_gearStrokeWidth, 1);
 			strokeColor = typedArray.getColor(R.styleable.GearView_gearStrokeColor, Color.WHITE);
 			showSideHole = typedArray.getBoolean(R.styleable.GearView_gearShowSideHole, true);
+			showCenterHole = typedArray.getBoolean(R.styleable.GearView_gearShowCenterHole, true);
 			if (direction == 1) {
 				directionRight = false;
 			}
@@ -99,10 +101,10 @@ public class GearView extends View {
 		height = getHeight();
 		lon = r = Math.min(width, height) / 2;
 		r = r - gearHeight / 2;
-		preDrawGear();
+		//		preDrawGear();
 	}
 	
-	private void preDrawGear() {
+	/*private void preDrawGear() {
 		gear = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 		paintFill.setXfermode(null);
 		
@@ -110,7 +112,7 @@ public class GearView extends View {
 		gearCanvas.translate(width / 2, height / 2);
 		
 		path.reset();
-		int    count    = gearToothCount;
+		int    count    = gearToothCount * 4;
 		double perRadio = Math.PI / count * 2;
 		
 		PointF p0 = getRadioPointPI(perRadio, r);
@@ -131,7 +133,7 @@ public class GearView extends View {
 		if (showSideHole) {
 			int c = 5;
 			for (int i = 0; i < c; i++) {
-				PointF p = getRadioPoint(45 + (i * 360 / c), r / 2);
+				PointF p = getRadioPoint(45.0 + (i * 360.0 / c), r / 2.0);
 				paintFill.setXfermode(xfermode);
 				gearCanvas.drawCircle(p.x, p.y, r / c, paintFill);
 				paintFill.setXfermode(null);
@@ -139,13 +141,14 @@ public class GearView extends View {
 			}
 		}
 		
-		paintFill.setXfermode(xfermode);
-		gearCanvas.drawCircle(0, 0, r / 6 / 2, paintFill);
-		paintFill.setXfermode(null);
-		gearCanvas.drawCircle(0, 0, r / 6 / 2, paintStroke);
-		paintFill.setColor(gearColor);
-		
-	}
+		if (showCenterHole) {
+			paintFill.setXfermode(xfermode);
+			gearCanvas.drawCircle(0, 0, (float) (gearHeight / 2.0), paintFill);
+			paintFill.setXfermode(null);
+			gearCanvas.drawCircle(0, 0, (float) (gearHeight / 2.0), paintStroke);
+			paintFill.setColor(gearColor);
+		}
+	}*/
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
@@ -154,12 +157,61 @@ public class GearView extends View {
 		if (startTime != 0) {
 			radio = (System.currentTimeMillis() - startTime) / 1000.0 * ((directionRight) ? perDegree : -perDegree);
 		}
-		transferRadio = transferRadio + radio;
 		startTime = System.currentTimeMillis();
-		canvas.rotate((float) transferRadio + startDegree, width / 2, height / 2);
+		transferRadio = transferRadio + radio;
+		//		canvas.rotate((float) transferRadio + startDegree, width / 2, height / 2);
+		//		paintFill.setColor(gearColor);
+		//		canvas.drawBitmap(gear, 0, 0, null);
+		//		invalidate();
+		paintFill.setColor(Color.BLACK);
+		canvas.saveLayer(0, 0, width, height, paintFill);
+		
+		paintFill.setXfermode(null);
+		
+		//		canvas = new Canvas(gear);
+		canvas.translate(width / 2, height / 2);
+		canvas.rotate((float) transferRadio + startDegree, 0, 0);
+		
+		path.reset();
+		int    count    = gearToothCount * 4;
+		double perRadio = Math.PI / count * 2;
+		
+		PointF p0 = getRadioPointPI(perRadio, r);
+		path.moveTo(p0.x, p0.y);
+		
+		for (int i = 2; i <= count; i += 2) {
+			double radio1      = perRadio * i;
+			double radio2      = perRadio * (i + 1);
+			PointF pController = getRadioPointPI(radio1, r + ((i % 4 == 0) ? -gearHeight : gearHeight));
+			PointF p2          = getRadioPointPI(radio2, r);
+			path.quadTo(pController.x, pController.y, p2.x, p2.y);
+		}
+		
 		paintFill.setColor(gearColor);
-		canvas.drawBitmap(gear, 0, 0, null);
+		canvas.drawPath(path, paintFill);
+		canvas.drawPath(path, paintStroke);
+		paintFill.setColor(Color.BLACK);
+		if (showSideHole) {
+			int c = 5;
+			for (int i = 0; i < c; i++) {
+				PointF p = getRadioPoint(45.0 + (i * 360.0 / c), r / 2.0);
+				paintFill.setXfermode(xfermode);
+				canvas.drawCircle(p.x, p.y, r / c, paintFill);
+				paintFill.setXfermode(null);
+				canvas.drawCircle(p.x, p.y, r / c, paintStroke);
+			}
+		}
+		
+		if (showCenterHole) {
+			paintFill.setXfermode(xfermode);
+			canvas.drawCircle(0, 0, (float) (gearHeight / 2.0), paintFill);
+			paintFill.setXfermode(null);
+			canvas.drawCircle(0, 0, (float) (gearHeight / 2.0), paintStroke);
+			paintFill.setColor(gearColor);
+		}
+		canvas.restore();
 		invalidate();
+		
 	}
 	
 	private static PointF getRadioPointPI(double radioOdPI, float r) {
@@ -171,7 +223,7 @@ public class GearView extends View {
 		return point;
 	}
 	
-	private static PointF getRadioPoint(double radio, float r) {
+	private static PointF getRadioPoint(double radio, double r) {
 		PointF point = new PointF();
 		double x, y;
 		x = r * Math.cos(radio / 180 * Math.PI);
